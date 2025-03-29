03c005463afa81148aa22cd27fc978003a97b871
RomanosSrt <rpsarantidis@gmail.com>
2025-03-29 13:21:13 +0200

    TTS fix
    seekbar tracks sentences, narrator in Greek with no issue

--- app/src/main/java/com/unipi/talescast/PlayActivity.java
+++ app/src/main/java/com/unipi/talescast/PlayActivity.java
@@ -3,19 +3,19 @@ package com.unipi.talescast;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.Voice;
+import android.speech.tts.UtteranceProgressListener;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Locale;
 
@@ -23,13 +23,37 @@ public class PlayActivity extends AppCompatActivity {
     CardModel cardSelected;
     TextToSpeech narrator;
     Button playButton;
+    SeekBar seekBar;
     boolean stop = false;
+    int playFrom;
+    Handler handler = new Handler();
+    Runnable progressLoop;
+    String[] storyTable;
+    int progress = 0;
+    int maxProgress;
+    final int INTERVAL = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_play);
+        seekBar = findViewById(R.id.seekBar);
+        playButton = findViewById(R.id.playButton);
+        TextView titleText = findViewById(R.id.titleText);
+        cardSelected = (CardModel) getIntent().getSerializableExtra("card");
+
+
+        if (cardSelected != null) {
+            storyTable = cardSelected.story.trim().split("(?<=[,.?!])\\s+");
+            titleText.setText(cardSelected.title);
+            seekBar.setMax(storyTable.length);
+            maxProgress = seekBar.getMax();
+            setImage(cardSelected.image);
+        } else {
+            finish();
+        }
+
         narrator = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int i) {
@@ -39,38 +63,92 @@ public class PlayActivity extends AppCompatActivity {
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        cardSelected = (CardModel) getIntent().getSerializableExtra("card");
-
-        if (cardSelected != null) {
-            TextView titleText = findViewById(R.id.titleText);
-            titleText.setText(cardSelected.title);
-
-            // Example: Decode the image
-            ImageView imageView = findViewById(R.id.taleImage);
-            byte[] bytes = Base64.decode(cardSelected.image, Base64.DEFAULT);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            imageView.setImageBitmap(bitmap);
-        } else {
-
-        }
-        playButton = findViewById(R.id.playButton);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (stop)
-                    narrator.speak(cardSelected.story, TextToSpeech.QUEUE_FLUSH, null);
-                else
+                if (!stop) {
+                    narrate();
+                }
+                else {
                     narrator.stop();
+                    narrator.shutdown();
+                }
+
                 stop = !stop;
             }
         });
 
+        narrator.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+                int index = Integer.parseInt(utteranceId.split("_")[1]);
+                runOnUiThread(() -> {
+                    seekBar.setProgress(index);
+                    titleText.setText(String.valueOf(index + 1));
+                });
+            }
+
+            @Override
+            public void onDone(String utteranceId) {}
+
+            @Override
+            public void onError(String utteranceId) {}
+        });
+
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                narrator.stop();
+                narrator.shutdown();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                playFrom = seekBar.getProgress();
+//                int continueFrom = cardSelected.story.indexOf(storyTable[playFrom]);
+                String restOfTheStory = cardSelected.story.substring(playFrom);
+                if (!stop) {
+//                    trackTale();
+                    narrator.speak(restOfTheStory, TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        narrator.stop();
+        narrator.shutdown();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        narrator.stop();
+        narrator.shutdown();
+    }
+
+    private void setImage(String b64){
+        ImageView imageView = findViewById(R.id.taleImage);
+        byte[] bytes = Base64.decode(b64, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private void narrate() {
+        playFrom = seekBar.getProgress();
+        for (int i = playFrom; i < storyTable.length; i++) {
+            String id = "sentence_" + i;
+            narrator.speak(storyTable[i], TextToSpeech.QUEUE_ADD, null, id);
+        }
     }
 }
